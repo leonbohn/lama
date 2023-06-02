ef482cd897e683ea3591e24d57aa93b04f4cf23e
León Bohn <bohn@lics.rwth-aachen.de>
2023-06-02 12:24:48 +0200

    WIP still, but reformat

--- automata/src/operations/product.rs
+++ automata/src/operations/product.rs
@@ -8,10 +8,11 @@ use crate::{
     output::{Assignment, AssignmentReference, IntoAssignments, Mapping},
     ts::{
         transitionsystem::{States, Transitions},
-        HasInput, HasStates, IntoTransitions, TransitionReference, TriggerOf, StateOf, InputOf, IntoStates, StateReference, IntoParts,
+        HasInput, HasStates, InputOf, IntoParts, IntoStates, IntoTransitions, StateOf,
+        StateReference, TransitionReference, TriggerOf,
     },
-    Acceptor, Combined, IntoMealyTransitions, MealyMachine, Pair, Pointed, State, Successor,
-    Symbol, Transformer, Transition, TransitionSystem, Trigger, Value, DBA, DFA, DPA, Equivalent,
+    Acceptor, Combined, Equivalent, IntoMealyTransitions, MealyMachine, Pair, Pointed, State,
+    Successor, Symbol, Transformer, Transition, TransitionSystem, Trigger, Value, DBA, DFA, DPA,
 };
 
 pub type AssignmentProductMapping<X, Y, A> =
@@ -56,7 +57,11 @@ pub struct Product<L, R> {
     right: R,
 }
 
-impl<L, R> Product<L, R> where L: Successor, R: Successor<Sigma = L::Sigma> {
+impl<L, R> Product<L, R>
+where
+    L: Successor,
+    R: Successor<Sigma = L::Sigma>,
+{
     /// Creates a new [`Product`] from the given elements.
     pub fn new(left: L, right: R) -> Self {
         Self { left, right }
@@ -70,8 +75,7 @@ where
 {
     type Sigma = L::Sigma;
 
-    type Input<'me> = L::Input<'me> 
-    where Self:'me;
+    type Input<'me> = L::Input<'me> where Self:'me;
 
     fn input_alphabet(&self) -> Self::Input<'_> {
         self.left.input_alphabet()
@@ -85,8 +89,9 @@ where
 {
     type Q = Pair<L::Q, R::Q>;
 
-    fn contains_state<X:Borrow<Self::Q>>(&self,state:X) -> bool {
-        self.left.contains_state(state.borrow().left()) && self.right.contains_state(state.borrow().right())
+    fn contains_state<X: Borrow<Self::Q>>(&self, state: X) -> bool {
+        self.left.contains_state(state.borrow().left())
+            && self.right.contains_state(state.borrow().right())
     }
 }
 
@@ -95,7 +100,11 @@ where
     L: Successor,
     R: Successor<Sigma = L::Sigma>,
 {
-    fn successor<X:Borrow<Self::Q>,Y:Borrow<Self::Sigma>>(&self,from:X,on:Y,) -> Option<Self::Q> {
+    fn successor<X: Borrow<Self::Q>, Y: Borrow<Self::Sigma>>(
+        &self,
+        from: X,
+        on: Y,
+    ) -> Option<Self::Q> {
         let Pair { left, right } = from.borrow();
         let sym = on.borrow();
         self.left
@@ -104,26 +113,38 @@ where
     }
 }
 
-impl<L, R> Pointed for Product<L, R> where L: Pointed, R: Pointed<Sigma = L::Sigma> {
+impl<L, R> Pointed for Product<L, R>
+where
+    L: Pointed,
+    R: Pointed<Sigma = L::Sigma>,
+{
     fn initial(&self) -> Self::Q {
         Pair::new(self.left.initial(), self.right.initial())
     }
 }
 
-pub struct ProductTransitions<L, R> where L: Iterator {
+pub struct ProductTransitions<L, R>
+where
+    L: Iterator,
+{
     left: L,
     left_element: Option<L::Item>,
     right: R,
     right_backup: R,
 }
 
-impl<L, R> Iterator for ProductTransitions<L, R> where
-L: Iterator,
-R: Iterator + Clone,
-L::Item: Clone + Transition,
-R::Item: Transition<S = <L::Item as Trigger>::S>,
+impl<L, R> Iterator for ProductTransitions<L, R>
+where
+    L: Iterator,
+    R: Iterator + Clone,
+    L::Item: Clone + Transition,
+    R::Item: Transition<S = <L::Item as Trigger>::S>,
 {
-    type Item = (Pair<<L::Item as Trigger>::Q, <R::Item as Trigger>::Q>, <L::Item as Trigger>::S, Pair<<L::Item as Trigger>::Q, <R::Item as Trigger>::Q>); 
+    type Item = (
+        Pair<<L::Item as Trigger>::Q, <R::Item as Trigger>::Q>,
+        <L::Item as Trigger>::S,
+        Pair<<L::Item as Trigger>::Q, <R::Item as Trigger>::Q>,
+    );
 
     fn next(&mut self) -> Option<Self::Item> {
         if let Some(left_element) = self.left_element.clone() {
@@ -141,7 +162,17 @@ R::Item: Transition<S = <L::Item as Trigger>::S>,
                 }
             };
             // trace!("\tEmitting: <{:?},{:?}> -{:?}-> <{:?},{:?}>", left_element.source(), right_element.source(), left_element.sym(), left_element.target(), right_element.target());
-            Some((Pair::new(left_element.source().clone(), right_element.source().clone()), left_element.sym().clone(), Pair::new(left_element.target().clone(), right_element.target().clone())))
+            Some((
+                Pair::new(
+                    left_element.source().clone(),
+                    right_element.source().clone(),
+                ),
+                left_element.sym().clone(),
+                Pair::new(
+                    left_element.target().clone(),
+                    right_element.target().clone(),
+                ),
+            ))
         } else {
             // trace!("left_element is None");
             None
@@ -149,10 +180,14 @@ R::Item: Transition<S = <L::Item as Trigger>::S>,
     }
 }
 
-pub fn product_transitions<L, R>(left_transitions: L, right_transitions: R) -> ProductTransitions<L::IntoTransitions, R::IntoTransitions> where
-L: IntoTransitions,
-R: IntoTransitions<Sigma = L::Sigma>,
-R::IntoTransitions: Clone,
+pub fn product_transitions<L, R>(
+    left_transitions: L,
+    right_transitions: R,
+) -> ProductTransitions<L::IntoTransitions, R::IntoTransitions>
+where
+    L: IntoTransitions,
+    R: IntoTransitions<Sigma = L::Sigma>,
+    R::IntoTransitions: Clone,
 {
     let mut left = left_transitions.into_transitions();
     let right = right_transitions.into_transitions();
@@ -164,21 +199,24 @@ R::IntoTransitions: Clone,
     }
 }
 
-
-pub struct ProductStates<L, R> where L: Iterator {
+pub struct ProductStates<L, R>
+where
+    L: Iterator,
+{
     left: L,
     left_element: Option<L::Item>,
     right: R,
     right_backup: R,
 }
 
-impl<L, R> Iterator for ProductStates<L, R> where
-L: Iterator,
-R: Iterator + Clone,
-L::Item: Clone + StateReference,
-R::Item: StateReference,
+impl<L, R> Iterator for ProductStates<L, R>
+where
+    L: Iterator,
+    R: Iterator + Clone,
+    L::Item: Clone + StateReference,
+    R::Item: StateReference,
 {
-    type Item = Pair<<L::Item as StateReference>::Q, <R::Item as StateReference>::Q>; 
+    type Item = Pair<<L::Item as StateReference>::Q, <R::Item as StateReference>::Q>;
 
     fn next(&mut self) -> Option<Self::Item> {
         if let Some(left_element) = self.left_element.clone() {
@@ -188,18 +226,22 @@ R::Item: StateReference,
                     self.right = self.right_backup.clone();
                     self.left_element = self.left.next();
                     return self.next();
-                },
+                }
             };
             // trace!("\tEmitting: <{:?},{:?}> -{:?}-> <{:?},{:?}>", left_element.source(), right_element.source(), left_element.sym(), left_element.target(), right_element.target());
-            Some(Pair::new(left_element.state(), right_element.state()) )
+            Some(Pair::new(left_element.state(), right_element.state()))
         } else {
             // trace!("left_element is None");
             None
         }
     }
 }
-impl<'a, L, R> IntoStates for &'a Product<L, R> where L: IntoStates, R: IntoStates<Sigma = L::Sigma>,
-R::IntoStates: Clone {
+impl<'a, L, R> IntoStates for &'a Product<L, R>
+where
+    L: IntoStates,
+    R: IntoStates<Sigma = L::Sigma>,
+    R::IntoStates: Clone,
+{
     type StateRef = Pair<L::Q, R::Q>;
 
     type IntoStates = ProductStates<L::IntoStates, R::IntoStates>;
@@ -208,17 +250,25 @@ R::IntoStates: Clone {
         let mut left = self.left.into_states();
         let right = self.right.into_states();
         ProductStates {
-        left_element: left.next(),
-        left,
-        right_backup: right.clone(),
-        right,
-    }
+            left_element: left.next(),
+            left,
+            right_backup: right.clone(),
+            right,
+        }
     }
 }
 
-impl<'a, L, R> IntoTransitions for &'a Product<L, R> where L: IntoTransitions, R: IntoTransitions<Sigma = L::Sigma>,
-R::IntoTransitions: Clone, {
-    type TransitionRef = (Pair<StateOf<L>, StateOf<R>>, InputOf<L>, Pair<StateOf<L>, StateOf<R>>);
+impl<'a, L, R> IntoTransitions for &'a Product<L, R>
+where
+    L: IntoTransitions,
+    R: IntoTransitions<Sigma = L::Sigma>,
+    R::IntoTransitions: Clone,
+{
+    type TransitionRef = (
+        Pair<StateOf<L>, StateOf<R>>,
+        InputOf<L>,
+        Pair<StateOf<L>, StateOf<R>>,
+    );
 
     type IntoTransitions = ProductTransitions<L::IntoTransitions, R::IntoTransitions>;
 
@@ -227,29 +277,38 @@ R::IntoTransitions: Clone, {
     }
 }
 
-impl<L, R> Transformer for Product<L, R> where L: Transformer, R: Transformer {
+impl<L, R> Transformer for Product<L, R>
+where
+    L: Transformer,
+    R: Transformer,
+{
     type Domain = Pair<L::Domain, R::Domain>;
 
     type Range = Pair<L::Range, R::Range>;
 
-    fn apply<X: Borrow<Self::Domain>>(&self, input:X) -> Self::Range {
-        let Pair {left, right } = input.borrow();
+    fn apply<X: Borrow<Self::Domain>>(&self, input: X) -> Self::Range {
+        let Pair { left, right } = input.borrow();
         Pair::new(self.left.apply(left), self.right.apply(right))
     }
 }
 
-pub struct ProductAssignments<L, R> where L: Iterator {
+pub struct ProductAssignments<L, R>
+where
+    L: Iterator,
+{
     left: L,
     left_element: Option<L::Item>,
     right: R,
     right_backup: R,
 }
 
-impl<'a, L, R> IntoAssignments for &'a Product<L, R> where
-L: IntoAssignments,
-R: IntoAssignments,
-R::Assignments: Clone,
-L::AssignmentRef: Clone {
+impl<'a, L, R> IntoAssignments for &'a Product<L, R>
+where
+    L: IntoAssignments,
+    R: IntoAssignments,
+    R::Assignments: Clone,
+    L::AssignmentRef: Clone,
+{
     type AssignmentRef = (Pair<L::Domain, R::Domain>, Pair<L::Range, R::Range>);
 
     type Assignments = ProductAssignments<L::Assignments, R::Assignments>;
@@ -266,13 +325,17 @@ L::AssignmentRef: Clone {
     }
 }
 
-impl<L, R> Iterator for ProductAssignments<L, R> where
-L: Iterator,
-R: Iterator + Clone,
-L::Item: Clone + Assignment,
-R::Item: Assignment,
+impl<L, R> Iterator for ProductAssignments<L, R>
+where
+    L: Iterator,
+    R: Iterator + Clone,
+    L::Item: Clone + Assignment,
+    R::Item: Assignment,
 {
-    type Item = (Pair<<L::Item as Assignment>::Left, <R::Item as Assignment>::Left>, Pair<<L::Item as Assignment>::Right, <R::Item as Assignment>::Right>);
+    type Item = (
+        Pair<<L::Item as Assignment>::Left, <R::Item as Assignment>::Left>,
+        Pair<<L::Item as Assignment>::Right, <R::Item as Assignment>::Right>,
+    );
 
     fn next(&mut self) -> Option<Self::Item> {
         let right_element = match self.right.next() {
@@ -286,9 +349,14 @@ R::Item: Assignment,
                     }
                 }
             }
-            Some(a) => a
+            Some(a) => a,
         };
-        self.left_element.as_ref().map(|a| (Pair::new(a.left(), right_element.left()), Pair::new(a.right(), right_element.right())))
+        self.left_element.as_ref().map(|a| {
+            (
+                Pair::new(a.left(), right_element.left()),
+                Pair::new(a.right(), right_element.right()),
+            )
+        })
     }
 }
 
@@ -414,8 +482,8 @@ impl<Q: State, S: Symbol, A: Value> MealyMachine<A, Q, S> {
 mod tests {
     use crate::{
         output::{IntoAssignments, Mapping},
-        ts::{IntoTransitions, Visitor, IntoParts},
-        MealyMachine, Pair, Transformer, TransitionSystem, DFA, Successor,
+        ts::{IntoParts, IntoTransitions, Visitor},
+        MealyMachine, Pair, Successor, Transformer, TransitionSystem, DFA,
     };
 
     #[test]
