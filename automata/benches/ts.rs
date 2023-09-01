6a976aedd3b1e955b1451c30e7e9b014f77ad75b
León Bohn <bohn@lics.rwth-aachen.de>
2023-09-01 17:44:00 +0200

    unrandom benchmarks, use fxhash

--- automata/benches/ts.rs
+++ automata/benches/ts.rs
@@ -1,75 +1,67 @@
-use std::time::Duration;
-
 use automata::alphabet::Simple;
 use automata::word::Normalized;
 use automata::{simple, ts::Sproutable};
 use automata::{InfiniteLength, MooreMachine, Pointed, Successor};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use pprof::criterion::{Output, PProfProfiler};
-use rand::rngs::SmallRng;
-use rand::{thread_rng, Rng, SeedableRng};
 
-fn pseudorandom_sprout(states: usize, b: usize, r: usize) -> MooreMachine<Simple, usize> {
+fn pseudorandom_sprout(states: usize, n: usize) -> MooreMachine<Simple, usize> {
     let alphabet = simple!('a', 'b', 'c', 'd');
 
     let mut ts = automata::MooreMachine::new(alphabet);
 
     for i in 0..states {
-        let state = ts.add_state(i);
+        ts.add_state(i);
     }
 
-    let mut c = b;
+    let mut c: usize = 0;
     for sym in ['a', 'b', 'c', 'd'] {
         for i in 0..states {
-            ts.add_edge(i, sym, c % states, ());
-            c = (b.wrapping_mul(c.wrapping_add(r))).wrapping_add(r);
+            ts.add_edge(i, sym, c, ());
+            c = c.wrapping_add(n) % BENCH_SIZE;
         }
     }
     ts
 }
 
-const BENCH_SIZE: usize = 50;
-const B_RANGE: (usize, usize) = (1, 343);
-const R_RANGE: (usize, usize) = (664, 234873);
+const BENCH_SIZE: usize = 101;
 
-fn give_rand() -> Vec<(usize, usize)> {
-    let mut rng = thread_rng();
-    let mut values = Vec::with_capacity(BENCH_SIZE);
-    for _ in 0..BENCH_SIZE {
-        values.push((
-            rng.gen_range(B_RANGE.0..B_RANGE.1),
-            rng.gen_range(R_RANGE.0..R_RANGE.1),
-        ));
-    }
-    values
-}
-
-fn random_automata(rand: &[(usize, usize)]) -> Vec<MooreMachine<Simple, usize>> {
+fn random_automata(rand: &[usize]) -> Vec<MooreMachine<Simple, usize>> {
     let mut automata = vec![];
-    for (b, r) in rand {
-        automata.push(pseudorandom_sprout(BENCH_SIZE, *b, *r));
+    for n in rand {
+        automata.push(pseudorandom_sprout(BENCH_SIZE, *n));
     }
     automata
 }
 
 fn random_words() -> Vec<Vec<char>> {
-    let mut rng = thread_rng();
     let mut words = Vec::with_capacity(BENCH_SIZE);
-    for _ in 0..BENCH_SIZE {
+    for i in &RANDOM {
+        let mut n = i % 4;
         let mut word = Vec::with_capacity(100);
-        for _ in 0..rng.gen_range(2..BENCH_SIZE) {
-            word.push(rng.gen_range('a'..='d'));
+        for _ in 0..(i % BENCH_SIZE) {
+            word.push(['a', 'b', 'c', 'd'][n]);
+            n = n.wrapping_add(*i) % 4;
         }
         words.push(word);
     }
     words
 }
 
-fn bench_sprout(c: &mut Criterion) {
-    let random = give_rand();
+const RANDOM: [usize; 101] = [
+    907898, 628828, 656295, 585375, 168920, 976400, 283751, 146218, 924485, 485174, 551114, 414085,
+    23398, 692116, 364224, 527697, 329465, 180859, 197989, 675687, 986737, 697541, 2838, 341621,
+    376278, 981507, 691163, 350350, 974365, 976987, 958451, 608452, 624096, 114510, 463275, 406791,
+    877022, 125325, 820858, 493857, 835533, 311687, 33494, 188883, 816449, 373667, 399281, 797929,
+    771335, 642879, 95832, 331000, 734027, 964638, 470433, 843028, 324676, 644087, 942716, 228245,
+    480167, 637038, 822758, 827323, 190392, 607448, 541022, 250185, 35246, 266351, 323862, 30555,
+    691076, 148060, 200864, 387120, 887100, 112734, 970894, 548249, 898204, 833388, 661808, 304235,
+    233794, 405895, 324820, 69682, 455210, 860162, 408786, 51135, 944212, 639854, 261334, 624493,
+    663173, 525156, 295525, 775720, 373995,
+];
 
+fn bench_sprout(c: &mut Criterion) {
     c.bench_function("random_sprout 100", |b| {
-        b.iter(|| random_automata(black_box(&random)))
+        b.iter(|| random_automata(black_box(&RANDOM)))
     });
 }
 
@@ -88,6 +80,8 @@ fn bench_run(c: &mut Criterion) {
             }
         }
     }
+
+    #[allow(clippy::type_complexity)]
     fn infinite_run_words(
         (automata, words): (
             &[MooreMachine<Simple, usize>],
@@ -100,6 +94,7 @@ fn bench_run(c: &mut Criterion) {
             }
         }
     }
+    #[allow(clippy::type_complexity)]
     fn infinite_run_words_new(
         (automata, words): (
             &[MooreMachine<Simple, usize>],
@@ -120,12 +115,16 @@ fn bench_run(c: &mut Criterion) {
         }
     }
 
-    let random_automata = random_automata(&give_rand());
+    let random_automata = random_automata(&RANDOM);
     let random_words = random_words();
     let mut random_loops = Vec::new();
-    for word in &random_words {
-        let i = thread_rng().gen_range(0..(word.len().saturating_sub(1)));
-        random_loops.push(Normalized::new_omega(word, InfiniteLength(word.len(), i)))
+    for (i, n) in RANDOM.iter().enumerate() {
+        let word = &random_words[*n % BENCH_SIZE];
+        let j = std::cmp::min(
+            n.wrapping_mul(i + 3847) % word.len(),
+            word.len().saturating_sub(1),
+        );
+        random_loops.push(Normalized::new_omega(word, InfiniteLength(word.len(), j)))
     }
 
     c.bench_function("finite_random_runs 100", |b| {
