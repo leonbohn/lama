db1aeee48cad212f88a3553ae1bd933e0e0a048d
fesemeyer <felix.sebastian.meyer@rwth-aachen.de>
2024-03-12 11:33:50 +0100

    docs

--- automata-learning/src/passive/sprout.rs
+++ automata-learning/src/passive/sprout.rs
@@ -2,9 +2,14 @@ use automata::{automaton::Buchi, prelude::*};
 
 use super::OmegaSample;
 
+/// This trait represents the sprout passive learning algorithm for omega automata 
+/// from <https://arxiv.org/pdf/2108.03735.pdf>.
+/// The trait is supposed to be implemented on omega acceptance types
 pub trait SproutLearner {
+    /// type of the automaton to be returned
     type Aut;
 
+    /// gives a deterministic omega automaton that is consistent with the given sample
     fn sprout(sample: OmegaSample) -> Self::Aut;
 }
 
