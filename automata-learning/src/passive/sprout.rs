becdf34b747496762325ab6b03dda617d46fdb85
fesemeyer <felix.sebastian.meyer@rwth-aachen.de>
2024-03-12 10:45:04 +0100

    dummy implementation of SproutLearner for Buchi

--- automata-learning/src/passive/sprout.rs
+++ automata-learning/src/passive/sprout.rs
@@ -1,7 +1,17 @@
+use automata::{automaton::Buchi, prelude::*};
+
 use super::OmegaSample;
 
 pub trait SproutLearner {
     type Aut;
 
     fn sprout(sample: OmegaSample) -> Self::Aut;
+}
+
+impl SproutLearner for Buchi {
+    type Aut = DBA;
+
+    fn sprout(sample: OmegaSample) -> Self::Aut {
+        todo!()
+    }
 }
